18f016156e61a3e931637c03ea5f72844134a053
Rodrigo Caldas <c4ldas@gmail.com>
2024-05-08 03:53:59 +0100

    tested params and searchParams

--- app/(frontend)/lol/page.jsx
+++ app/(frontend)/lol/page.jsx
@@ -1,9 +1,15 @@
 import styles from "../page.module.css";
 
-export default function Home() {
+// http://localhost:3000/lol?hello=my%20friend
+
+export default function Home({ params, searchParams }) {
+
+  console.log(params);        // {}
+  console.log(searchParams);  // { hello: 'my friend' }
+
   return (
     <main className={styles.main}>
-      <div>This is the lol page</div>
+      <h1>{searchParams.hello}</h1>
     </main>
   );
 }
