86b7d1e41d51c4d145705181458130b9966ee6b8
Rodrigo Caldas <c4ldas@gmail.com>
2024-05-17 22:33:39 +0100

    Added example files

--- app/api/youtube/route.js
+++ app/api/youtube/route.js
@@ -1,6 +1,15 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request) {
-  console.log(request.nextUrl.pathname);
+  const origin = request.nextUrl.origin;
+  const pathname = request.nextUrl.pathname;
 
-  return Response.json(request.nextUrl.pathname);
-
-}
+  // console.log(request)
+  return NextResponse.json(
+    {
+      Usage: `${origin}${pathname}/youtube/<id>?type=TYPE`,
+      info: `<id> can be retrieved on ${origin}/spotify/login`,
+      type: ["json", "text"],
+    }
+  );
+};
