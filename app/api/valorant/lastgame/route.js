10c2818eab89213d583edb5a8f702739973fa3ee
Rodrigo Caldas <c4ldas@gmail.com>
2024-05-18 06:15:15 +0100

    Added frontend example

--- app/api/valorant/lastgame/route.js
+++ app/api/valorant/lastgame/route.js
@@ -4,16 +4,16 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
 
   // Convert query strings (map format) to object format - Only works for this specific case!
-  // const obj = Object.fromEntries(request.nextUrl.searchParams);
+  const obj = Object.fromEntries(request.nextUrl.searchParams);
   // obj.game = "lol";
   // const puuid = await getSummonerPuuid(obj)
   // console.log(puuid)
 
-  const id = "7cd4994f-3255-5575-b8a2-968f428bf9a1";
+  const id = obj.id
 
   const lastMatchRequest = await fetch(`https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/na/${id}?filter=competitive&size=1`, {
     headers: {
-      "Authorization": "HDEV-c079302a-3ba8-432a-9ce6-9d1adacf52f2"
+      "Authorization": process.env.VALORANT_TOKEN
     }
   });
 
@@ -21,13 +21,18 @@ export async function GET(request) {
   const data = lastMatch.data[0]
 
   const allPlayers = data.players.all_players;
-  const map = data.metadata.map
-  const gameStart = data.metadata.game_start
-  const gameDuration = data.metadata.game_length
-  const gameFinish = gameStart + gameDuration
-  const winner = (data.teams.blue.has_won == true) ? "Blue" : "Red"
-  const player = allPlayers.find((player) => player.puuid == id)
+  const map = data.metadata.map;
+  const gameStart = data.metadata.game_start;
+  const gameDuration = data.metadata.game_length;
+  const gameFinish = gameStart + gameDuration;
+  const winner = (data.teams.blue.has_won == true) ? "Blue" : "Red";
+  const player = allPlayers.find((player) => player.puuid == id);
+  player.hasWon = (winner == player.team);
+  player.gameDuration = gameDuration;
+  player.map = map;
+  player.rounds_played = data.metadata.rounds_played;
 
+  console.log(player.map)
   /*   console.log(`Map is ${map}`)
     console.log(`Winner is ${winner}`)
     console.log(`Result is ${(winner == player.team) ? "Win!" : "Lose :("}`)
@@ -35,8 +40,10 @@ export async function GET(request) {
     console.log(`Character is ${player.character}`)
     console.log(`KDA is ${player.stats.kills}/${player.stats.deaths}/${player.stats.assists}`); */
 
-
   // console.log(data)
+  if (obj.data == "full") {
+    return NextResponse.json(data);
+  }
   return NextResponse.json(player);
 }
 
