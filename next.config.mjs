7450dd5588d7d9a9abc8ec30614695c3c7e1caa1
Rodrigo Caldas <c4ldas@gmail.com>
2024-05-27 04:42:23 +0100

    Testing headers for cors issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },
+
+
   images: {
     remotePatterns: [
       {
