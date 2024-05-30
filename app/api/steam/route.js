c2cdfc19e600c7688fcca4b656dd365d44ac920c
Rodrigo Caldas <c4ldas@gmail.com>
2024-05-30 09:39:13 +0100

    Added `/api/steam/game` endpoint

--- app/api/steam/route.js
+++ app/api/steam/route.js
@@ -7,7 +7,18 @@ export async function GET(request) {
   // console.log(request)
   return NextResponse.json(
     {
-
+      endpoint: `/api/steam/game/`,
+      usage: `${origin}${pathname}/game/?id=ID&region=REGION&type=TYPE`,
+      additional_info: "",
+      region: [
+        "ar", "at", "au", "be", "br", "cl", "cn", "ca", "co", "cr",
+        "cy", "ee", "es", "fi", "fr", "de", "gr", "hk", "id", "ie",
+        "il", "in", "it", "jp", "kz", "kw", "lt", "lu", "lv", "my",
+        "mt", "mx", "nl", "no", "nz", "pe", "ph", "pl", "pt", "qa",
+        "ru", "sg", "sk", "si", "za", "kr", "tw", "th", "tr", "ua",
+        "uk", "ae", "us", "uy", "vn"
+      ],
+      pending_implementation: "&type=TYPE",
       type: ["json", "text"],
     }
   );
