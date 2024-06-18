4521d1de5dc6b747daafed97e9e78bdbdd88e027
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-18 23:56:20 +0100

    Added `/api/spotify/musica/` endpoint

--- app/api/spotify/route.js
+++ app/api/spotify/route.js
@@ -4,12 +4,23 @@ export async function GET(request) {
   const origin = request.nextUrl.origin;
   const pathname = request.nextUrl.pathname;
 
-  // console.log(request)
-  return NextResponse.json(
+  return NextResponse.json([
     {
-      Usage: `${origin}${pathname}/musica/<id>?type=TYPE`,
-      info: `<id> can be retrieved on ${origin}/spotify/login`,
-      type: ["json", "text"],
-    }
-  );
-};
+      endpoint: `/api/spotify/musica/<id>?type=TYPE`,
+      description: "Shows which song is currently playing for the user",
+      usage: `${origin}${pathname}/musica/<id>/?type=TYPE`,
+      params: {
+        id: {
+          description: `ID of the user after registering on API. Can be retrieved on ${origin}/spotify/login`,
+          required: true,
+        },
+        type: {
+          description: "Format of the response",
+          required: false,
+          default: "text",
+          available_variables: ["json", "text"],
+        },
+      },
+    },
+  ]);
+}
