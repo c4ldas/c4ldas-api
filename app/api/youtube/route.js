966f6017b479e0fa8e5a3dcbd9a5d0b3e7e3e1ad
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-18 23:16:48 +0100

    Added `channel` and `search` endpoints

--- app/api/youtube/route.js
+++ app/api/youtube/route.js
@@ -4,12 +4,57 @@ export async function GET(request) {
   const origin = request.nextUrl.origin;
   const pathname = request.nextUrl.pathname;
 
-  // console.log(request)
-  return NextResponse.json(
+  return NextResponse.json([
     {
-      Usage: `${origin}${pathname}/youtube/<id>?type=TYPE`,
-      info: `<id> can be retrieved on ${origin}/spotify/login`,
-      type: ["json", "text"],
+      endpoint: `/api/youtube/channel/?username=USERNAME&type=TYPE`,
+      description: "Retrieve information about a YouTube channel.",
+      usage: `${origin}${pathname}/channel/?username=USERNAME&type=TYPE`,
+      params: {
+        username: {
+          description: "YouTube handle, also known as @username",
+          required: true,
+        },
+        type: {
+          description: "Format of the response",
+          required: false,
+          default: "json",
+          available_variables: ["json", "text"],
+        },
+      }
+    },
+    {
+      endpoint: `/api/youtube/search/<id>/?type=TYPE`,
+      description: "Retrieve information about a video from YouTube.",
+      usgae: `${origin}${pathname}/search/<id>/?type=TYPE`,
+      params: {
+        id: {
+          description: "Video ID",
+          required: true,
+        },
+        type: {
+          description: "Format of the response",
+          required: false,
+          default: "json",
+          available_variables: ["json", "text"],
+        },
+      }
+    },
+    {
+      endpoint: `/api/youtube/search?id=ID&type=TYPE`,
+      description: "Retrieve information about a video from YouTube.",
+      usage: `${origin}${pathname}/search/?id=ID&type=TYPE`,
+      params: {
+        id: {
+          description: "Video ID",
+          required: true,
+        },
+        type: {
+          description: "Format of the response",
+          required: false,
+          default: "json",
+          available_variables: ["json", "text"],
+        },
+      }
     }
-  );
+  ]);
 };
