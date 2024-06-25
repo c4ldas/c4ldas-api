82ede4c2e2633a4a97919f70b3039e610e9a071d
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-25 22:17:59 +0100

    Created spotify lib

--- app/api/spotify/callback/route.js
+++ app/api/spotify/callback/route.js
@@ -1,23 +1,6 @@
 import { NextResponse } from "next/server";
+import { getTokenCode, getUserData } from "@/app/lib/spotify";
 
-import decrypt from "@/app/lib/encode_key";
-import { color } from "@/app/lib/colorLog";
-
-const env = process.env.ENVIRONMENT;
-// const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
-const SPOTIFY_REDIRECT_URI = "http://localhost:3000/api/spotify/callback";
-
-let SPOTIFY_CLIENT_ID;
-let SPOTIFY_CLIENT_SECRET;
-
-if (env == "dev") {
-  SPOTIFY_CLIENT_ID = decrypt(process.env.SPOTIFY_CLIENT_ID);
-  SPOTIFY_CLIENT_SECRET = decrypt(process.env.SPOTIFY_CLIENT_SECRET);
-
-} else {
-  SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-  SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-}
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
@@ -26,8 +9,8 @@ export async function GET(request) {
 
   if (!code) return NextResponse.json({ error: "Code not found" }, { status: 400 });
 
-  const token = await getToken(code);
-  const user = await getUser(token.access_token);
+  const token = await getTokenCode(code);
+  const user = await getUserData(token.access_token);
 
   const data = {
     id: user.id,
@@ -36,82 +19,14 @@ export async function GET(request) {
     refresh_token: token.refresh_token,
   };
 
-  const saved = await saveToDatabase(data);
-  if (!saved) return NextResponse.json({ error: "Failed to save to database" }, { status: 500 });
+  // Pending 
+  // const saved = await saveToDatabase(data);
+  // if (!saved) return NextResponse.json({ error: "Failed to save to database" }, { status: 500 });
 
   return NextResponse.json({ id: user.id, display_name: user.display_name }, { status: 200 });
-
 }
 
-
-async function getToken(code) {
-  const getTokenFetch = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${Buffer.from(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET).toString("base64")}`,
-    },
-    body: new URLSearchParams({
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: SPOTIFY_REDIRECT_URI,
-    }),
-  });
-  const data = await getTokenFetch.json();
-  return data;
-
-  /*   
-  {
-    "access_token": "******",
-    "token_type": "Bearer",
-    "expires_in": 3600,
-    "refresh_token": "********",
-    "scope": "user-modify-playback-state user-read-currently-playing"
-  } 
-  */
-}
-
-async function getUser(token) {
-  const getUserFetch = await fetch("https://api.spotify.com/v1/me", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = await getUserFetch.json();
-  return data;
-
-  /* 
-    {
-      "display_name": "c4ldas",
-      "href": "https://api.spotify.com/v1/users/c4ldas",
-      "id": "c4ldas",
-      "type": "user",
-      "uri": "spotify:user:c4ldas",
-      "external_urls": {
-        "spotify": "https://open.spotify.com/user/c4ldas"
-      },
-      "followers": {
-        "href": null,
-        "total": 21
-      },
-      "images": [
-        {
-          "url": "https://i.scdn.co/image/ab67757000003b82d3ff308820a44bfa543ef303",
-          "height": 64,
-          "width": 64
-        },
-        {
-          "url": "https://i.scdn.co/image/ab6775700000ee85d3ff308820a44bfa543ef303",
-          "height": 300,
-          "width": 300
-        }
-      ]
-    }
-   */
-}
-
+// Pending function
 async function saveToDatabase(user) {
   // Table name: spotify
   // Columns: id, display_name, access_token, refresh_token
@@ -121,62 +36,3 @@ async function saveToDatabase(user) {
   if (failed) return false;
   return true;
 }
-
-/* 
-
-const fetch = require("node-fetch");
-const express = require("express");
-const router = express.Router();
-const Database = require("@replit/database");
-const db = new Database();
-
-router.get("/", async (req, res) => {
-  if (req.query.error) {
-    res.send(`Aplicação não foi aceita: ${req.query.error}`);
-    return;
-  }
-
-  const getTokenFetch = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString("base64")}`,
-    },
-    body: new URLSearchParams({
-      grant_type: "authorization_code",
-      code: req.query.code,
-      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-    }),
-  });
-  const getTokenJson = await getTokenFetch.json();
-
-  const me = await fetch("https://api.spotify.com/v1/me", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${getTokenJson.access_token}`,
-    },
-  });
-  const meFetch = await me.json();
-
-  const id = meFetch.id;
-  const displayName = meFetch.display_name;
-  const token = getTokenJson.access_token;
-  const refreshToken = getTokenJson.refresh_token;
-
-  const values = {
-    _displayName: displayName,
-    _token: token,
-    _refreshToken: refreshToken,
-  };
-
-  console.log(`ID: ${id}, displayName: ${displayName}`);
-  await db.set(id, values);
-  res.render(__dirname + "/callback.ejs", { displayName: displayName, id: id });
-});
-
-module.exports = router;
-
-
-
-*/
--- app/lib/spotify.js
+++ app/lib/spotify.js
@@ -2,6 +2,8 @@ import { sql } from '@vercel/postgres';
 import decrypt from "@/app/lib/encode_key";
 
 const env = process.env.ENVIRONMENT;
+// const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+const SPOTIFY_REDIRECT_URI = "http://localhost:3000/api/spotify/callback";
 let SPOTIFY_CLIENT_ID;
 let SPOTIFY_CLIENT_SECRET;
 
@@ -14,7 +16,7 @@ if (env == "dev") {
   SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 }
 
-export async function getRefreshTokenDatabase(id) {
+async function getRefreshTokenDatabase(id) {
   try {
 
     const refreshTokenQuery = {
@@ -37,7 +39,9 @@ export async function getRefreshTokenDatabase(id) {
   }
 }
 
-export async function getAccessToken(refreshToken) {
+
+
+async function getAccessToken(refreshToken) {
   try {
 
     const accessTokenRequest = await fetch('https://accounts.spotify.com/api/token', {
@@ -64,7 +68,9 @@ export async function getAccessToken(refreshToken) {
   }
 }
 
-export async function getSong(accessToken, type) {
+
+
+async function getSong(accessToken, type) {
   try {
 
     const musicFetch = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
@@ -87,4 +93,76 @@ export async function getSong(accessToken, type) {
   }
 }
 
-export async function getUserData(accessToken, type) { }
+
+
+async function getUserData(token) {
+  const getUserFetch = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await getUserFetch.json();
+  return data;
+
+  /* 
+    {
+      "display_name": "c4ldas",
+      "href": "https://api.spotify.com/v1/users/c4ldas",
+      "id": "c4ldas",
+      "type": "user",
+      "uri": "spotify:user:c4ldas",
+      "external_urls": {
+        "spotify": "https://open.spotify.com/user/c4ldas"
+      },
+      "followers": {
+        "href": null,
+        "total": 21
+      },
+      "images": [
+        {
+          "url": "https://i.scdn.co/image/ab67757000003b82d3ff308820a44bfa543ef303",
+          "height": 64,
+          "width": 64
+        },
+        {
+          "url": "https://i.scdn.co/image/ab6775700000ee85d3ff308820a44bfa543ef303",
+          "height": 300,
+          "width": 300
+        }
+      ]
+    }
+   */
+}
+
+
+
+async function getTokenCode(code) {
+  const getTokenFetch = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET).toString("base64")}`,
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: SPOTIFY_REDIRECT_URI,
+    }),
+  });
+  const data = await getTokenFetch.json();
+  return data;
+
+  /*   
+  {
+    "access_token": "******",
+    "token_type": "Bearer",
+    "expires_in": 3600,
+    "refresh_token": "********",
+    "scope": "user-modify-playback-state user-read-currently-playing"
+  } 
+  */
+}
+
+export { getRefreshTokenDatabase, getAccessToken, getSong, getUserData, getTokenCode };
