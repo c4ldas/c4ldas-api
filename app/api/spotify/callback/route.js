1ac4c6769060b5749d148e61fc1506626180b436
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-25 23:46:22 +0100

    Pending database function

--- app/api/spotify/callback/route.js
+++ app/api/spotify/callback/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { getTokenCode, getUserData } from "@/app/lib/spotify";
+import { sql } from "@vercel/postgres";
 
 
 export async function GET(request) {
@@ -21,18 +22,55 @@ export async function GET(request) {
 
   // Pending 
   // const saved = await saveToDatabase(data);
-  // if (!saved) return NextResponse.json({ error: "Failed to save to database" }, { status: 500 });
+  // if (!saved) return NextResponse.json({ error: "Failed to save to database, try again later." }, { status: 500 });
 
   return NextResponse.json({ id: user.id, display_name: user.display_name }, { status: 200 });
 }
 
 // Pending function
-async function saveToDatabase(user) {
-  // Table name: spotify
-  // Columns: id, display_name, access_token, refresh_token
+/* 
+async function saveToDatabase(data) {
 
-  // Save to database (pending)
+  const { id, display_name, access_token, refresh_token } = data;
 
-  if (failed) return false;
+  const selectQuery = {
+    text: 'SELECT refresh_token FROM spotify WHERE id = $1',
+    values: [id],
+  }
+  const insertQuery = {
+    text: 'INSERT INTO spotify (id, display_name, access_token, refresh_token) VALUES ($1, $2, $3, $4)',
+    values: [id, display_name, access_token, refresh_token],
+  }
+
+  const updateQuery = {
+    text: 'UPDATE spotify SET display_name = $2, access_token = $3, refresh_token = $4 WHERE id = $1',
+    values: [id, display_name, access_token, refresh_token],
+  }
+
+  const client = await sql.connect();
+  const { rows } = await client.query(selectQuery);
+  client.release();
+
+  console.log("Rows: ", rows);
+
+  if (!rows[0]) {
+    console.log("User not found! Inserting...");
+    const insert = await client.query(insertQuery);
+    console.log("Insert Query: ", insert.rows);
+    client.release();
+
+    if (insert.rows[0]) {
+      console.log("Inserted into database successfully!");
+      return true;
+    } else {
+      console.log("Failed to insert into database!");
+      return false;
+    }
+  }
+
   return true;
 }
+ */
+
+
+
