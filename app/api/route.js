3374b42073a7053d3e60bb3941232c8e222ef884
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-27 00:41:12 +0100

    Commented `NextResponse` import

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,8 @@
+// import { NextResponse } from 'next/server';
 
 export async function GET(request) {
 
   // console.log(request.nextUrl.origin);
   return Response.redirect(request.nextUrl.origin);
 }
+
