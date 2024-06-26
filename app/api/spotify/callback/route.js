ebf9ab687c923e77f1b0842ae0235e966a4d3191
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-26 23:44:34 +0100

    Removed `@vercel/postgres` library

--- app/api/spotify/callback/route.js
+++ app/api/spotify/callback/route.js
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
 import { getTokenCode, getUserData } from "@/app/lib/spotify";
-import { sql } from "@vercel/postgres";
-
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
@@ -26,51 +24,3 @@ export async function GET(request) {
 
   return NextResponse.json({ id: user.id, display_name: user.display_name }, { status: 200 });
 }
-
-// Pending function
-/* 
-async function saveToDatabase(data) {
-
-  const { id, display_name, access_token, refresh_token } = data;
-
-  const selectQuery = {
-    text: 'SELECT refresh_token FROM spotify WHERE id = $1',
-    values: [id],
-  }
-  const insertQuery = {
-    text: 'INSERT INTO spotify (id, display_name, access_token, refresh_token) VALUES ($1, $2, $3, $4)',
-    values: [id, display_name, access_token, refresh_token],
-  }
-
-  const updateQuery = {
-    text: 'UPDATE spotify SET display_name = $2, access_token = $3, refresh_token = $4 WHERE id = $1',
-    values: [id, display_name, access_token, refresh_token],
-  }
-
-  const client = await sql.connect();
-  const { rows } = await client.query(selectQuery);
-  client.release();
-
-  console.log("Rows: ", rows);
-
-  if (!rows[0]) {
-    console.log("User not found! Inserting...");
-    const insert = await client.query(insertQuery);
-    console.log("Insert Query: ", insert.rows);
-    client.release();
-
-    if (insert.rows[0]) {
-      console.log("Inserted into database successfully!");
-      return true;
-    } else {
-      console.log("Failed to insert into database!");
-      return false;
-    }
-  }
-
-  return true;
-}
- */
-
-
-
