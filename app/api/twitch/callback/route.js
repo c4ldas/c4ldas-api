f1f08a9f01f090756c07ef9d49ac1ae00841c269
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-29 07:47:18 +0100

    Created callback placeholder

--- app/api/twitch/callback/route.js
+++ app/api/twitch/callback/route.js
@@ -1,15 +1,42 @@
 import { NextResponse } from "next/server";
+import { twitchSaveToDatabase } from "@/app/lib/database";
+
+// Pending
+// import { getTokenCode, getUserData } from "@/lib/twitch";
 
 export async function GET(request) {
+  // Convert query strings (map format) to object format - Only works for this specific case!
+  const obj = Object.fromEntries(request.nextUrl.searchParams);
+  const { code } = obj;
   const origin = request.nextUrl.origin;
-  const pathname = request.nextUrl.pathname;
 
-  // console.log(request)
-  return NextResponse.json(
-    {
-      Usage: `${origin}${pathname}/twitch/<id>?type=TYPE`,
-      info: `<id> can be retrieved on ${origin}/twitch/login`,
-      type: ["json", "text"],
-    }
-  );
-};
+  if (!code) return NextResponse.json({ error: "Code not found" }, { status: 400 });
+
+  const token = await getTokenCode(code);
+  const user = await getUserData(token.access_token);
+
+  const data = {
+    id: user.id,
+    display_name: user.display_name,
+    access_token: token.access_token,
+    refresh_token: token.refresh_token,
+  };
+
+
+  const saved = await twitchSaveToDatabase(data);
+  if (!saved) return NextResponse.json({ error: "Failed to save to database, try again later." }, { status: 500 });
+
+  return Response.redirect(`${origin}/twitch?id=${user.id}&display_name=${user.display_name}`);
+
+}
+// If it is already registered, do not generate new code but save the new token information
+// code = userDb ? userDb.code : uuidv4().replace(/-/g, '');
+// code, username, id, access_token, refresh_token
+
+async function getTokenCode(code) {
+  return true
+}
+
+async function getUserData(token) {
+  return true
+}
