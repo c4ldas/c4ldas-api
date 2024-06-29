ad617169ceb4307d14b108aeea2d3c282e369d73
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-29 06:32:55 +0100

    Imported the functions from correct files

--- app/api/spotify/musica/route.js
+++ app/api/spotify/musica/route.js
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
-import { getRefreshTokenDatabase, getAccessToken, getSong } from "@/app/lib/spotify";
-import { sendResponse } from "./[id]/route";
+import { getAccessToken, getSong, sendResponse } from "@/app/lib/spotify";
+import { spotifyGetRefreshTokenDatabase } from "@/app/lib/database";
 
 export async function GET(request, { params }) {
   // Convert query strings (map format) to object format - Only works for this specific case!
   const obj = Object.fromEntries(request.nextUrl.searchParams);
   const { id, channel, type = "text" } = obj;
 
+
   try {
-    const refreshToken = await getRefreshTokenDatabase(id);
+    const refreshToken = await spotifyGetRefreshTokenDatabase(id);
     const accessToken = await getAccessToken(refreshToken, type);
     const song = await getSong(accessToken, type);
 
