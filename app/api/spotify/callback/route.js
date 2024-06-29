bc8adf656d60b296fd94194606b751b1424faef6
Rodrigo Caldas <c4ldas@gmail.com>
2024-06-29 06:32:20 +0100

    Added redirect back to the front page

--- app/api/spotify/callback/route.js
+++ app/api/spotify/callback/route.js
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 import { getTokenCode, getUserData } from "@/app/lib/spotify";
+import { spotifySaveToDatabase } from "@/app/lib/database";
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
   const obj = Object.fromEntries(request.nextUrl.searchParams);
   const { code } = obj;
+  const origin = request.nextUrl.origin;
 
   if (!code) return NextResponse.json({ error: "Code not found" }, { status: 400 });
 
@@ -18,9 +20,10 @@ export async function GET(request) {
     refresh_token: token.refresh_token,
   };
 
-  // Pending 
-  // const saved = await saveToDatabase(data);
-  // if (!saved) return NextResponse.json({ error: "Failed to save to database, try again later." }, { status: 500 });
 
-  return NextResponse.json({ id: user.id, display_name: user.display_name }, { status: 200 });
+  const saved = await spotifySaveToDatabase(data);
+  if (!saved) return NextResponse.json({ error: "Failed to save to database, try again later." }, { status: 500 });
+
+  return Response.redirect(`${origin}/spotify?id=${user.id}&display_name=${user.display_name}`);
+
 }
