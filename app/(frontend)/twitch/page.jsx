4ab5e66ad454a4f1a4ca4e2326027980d1c31e8b
Rodrigo Caldas <c4ldas@gmail.com>
2024-07-23 16:39:37 +0100

    Added cookies-next package

--- app/(frontend)/twitch/page.jsx
+++ app/(frontend)/twitch/page.jsx
@@ -2,17 +2,22 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { getCookies } from "cookies-next";
 import Header from "@/app/components/Header";
 import FooterComponent from "@/app/components/Footer";
 
-export default function Twitch({ params, searchParams }) {
+export default function Twitch({ _, searchParams }) {
+  const error = searchParams.error;
+  const path = usePathname();
+
+  const [cookie, setCookie] = useState({});
   const [origin, setOrigin] = useState();
   useEffect(() => {
     setOrigin(window.location.origin);
+    setCookie(getCookies());
   }, []);
 
-  const path = usePathname();
-  const { id, username, code, error } = searchParams;
+  // console.log("cookie:", cookie);
 
   const baseURL = 'https://id.twitch.tv/oauth2/authorize?'
   const urlSearchParams = new URLSearchParams({
@@ -23,24 +28,68 @@ export default function Twitch({ params, searchParams }) {
     force_verify: false
   });
 
+  async function openDialog() {
+    const dialog = document.querySelector("#dialog");
+    dialog.showModal();
+  }
+
+  function closeDialog() {
+    const dialog = document.querySelector("#dialog");
+    dialog.close();
+  }
+
+  async function confirmRemoval() {
+    const dialogTitle = document.querySelector("#dialog-title");
+    const submit = document.querySelector("#submit");
+    const cancel = document.querySelector("#cancel");
+    dialogTitle.innerText = "Removing integration, please wait...";
+    submit.style.display = "none";
+    cancel.style.display = "none";
+
+    setTimeout(async () => {
+      const request = await fetch("/api/twitch/logout", { "method": "POST" });
+      const response = await request.json();
+      dialogTitle.innerHTML = `${response.message}.<br/> Redirecting back to home page...`;
+    }, 1500);
+
+    setTimeout(() => {
+      window.location.assign("/twitch");
+    }, 3000);
+  }
+
   return (
     <div className="container">
       <Header />
       <main className="main block">
         <h1>This is the {path} page</h1>
-        {username && (
+        {cookie.id && (
           <>
-            <p>Display name: {username} </p>
-            <p>ID: {id}</p>
-            <p>Code: {code}</p>
+            <p><strong>Channel name:</strong> {cookie.username} </p>
+            <p><strong>Channel ID:</strong> {cookie.id}</p>
+            <details>
+              <summary>Click to show code:</summary>
+              <p><strong>{cookie.code}</strong></p>
+            </details>
+            <p><button id="remove-integration" type="submit" onClick={openDialog}>Remove integration</button></p>
+
+            { /* <!-- pop-up dialog box, containing a form --> */}
+            <dialog id="dialog" className="dialog">
+              <div id="dialog-title">
+                Are you sure you want to remove the integration?<br />
+                You can re-add it at any time.
+              </div>
+              <div id="dialog-buttons">
+                <button id="submit" type="submit" onClick={confirmRemoval}>Confirm</button>
+                <button id="cancel" type="reset" onClick={closeDialog}>Cancel</button>
+              </div>
+            </dialog>
           </>
         )}
         {error && <p>Error: {error}</p>}
-        {!username && (
+        {!cookie.id && (
           <a href={baseURL + urlSearchParams.toString()}>
             <button type="submit">Login with Twitch</button>
           </a>
-
         )}
       </main>
       <FooterComponent />
