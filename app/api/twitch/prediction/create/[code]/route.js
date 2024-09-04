8b9b8ebcaa6bfd51814e5fa4e00845558807edd1
Rodrigo Caldas <c4ldas@gmail.com>
2024-09-04 04:36:04 +0100

    Changed response status to 200

--- app/api/twitch/prediction/create/[code]/route.js
+++ app/api/twitch/prediction/create/[code]/route.js
@@ -13,15 +13,15 @@ export async function GET(request, { params }) {
       .filter((key) => key.startsWith("option"))
       .map(key => obj[key]);
 
-    if (!channel) return NextResponse.json({ status: "failed", error: "Channel missing" }, { status: 400 });
-    if (!question) return NextResponse.json({ status: "failed", error: "Question missing" }, { status: 400 });
-    if (!options || options.length < 2 || options.length > 10) return NextResponse.json({ status: "failed", error: "Invalid number of options. Min: 2, Max: 10" }, { status: 400 });
+    if (!channel) return NextResponse.json({ status: "failed", error: "Channel missing" }, { status: 200 });
+    if (!question) return NextResponse.json({ status: "failed", error: "Question missing" }, { status: 200 });
+    if (!options || options.length < 2 || options.length > 10) return NextResponse.json({ status: "failed", error: "Invalid number of options. Min: 2, Max: 10" }, { status: 200 });
 
     const token = await twitchGetTokenDatabase(code, channel);
-    if (!token) return NextResponse.json({ status: "failed", error: "Code and channel do not match" }, { status: 400 });
+    if (!token) return NextResponse.json({ status: "failed", error: "Code and channel do not match" }, { status: 200 });
     const openPrediction = await getOpenPrediction(token.access_token, token.id);
 
-    if (openPrediction) return NextResponse.json({ status: "failed", message: "Prediction already created" }, { status: 400 });
+    if (openPrediction) return NextResponse.json({ status: "failed", message: "Prediction already created" }, { status: 200 });
     const result = await createPrediction(token.access_token, token.id, question, options, time);
 
     return NextResponse.json(result, { status: 200 });
