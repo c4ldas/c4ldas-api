700df401a3baaa9b0d4335cebd54a9f4c0e82744
Rodrigo Caldas <c4ldas@gmail.com>
2024-12-13 00:05:00 +0000

    Added some comments

--- app/api/valorant/schedule/vlr/route.js
+++ app/api/valorant/schedule/vlr/route.js
@@ -20,17 +20,16 @@ import { Temporal } from "@js-temporal/polyfill";
 
 const vlrQuery = (query) => `https://vlrggapi.vercel.app/match?q=${query}`;
 const list = ["upcoming", "live_score", "results"];
-
 const validLeagues = {
   "tixinha_invitational": "Tixinha Invitational by BONOXS",
 }
-
-const timeZone = Temporal.TimeZone.from('America/Sao_Paulo'); // Using Brazil time zone
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
   const obj = Object.fromEntries(request.nextUrl.searchParams);
   const { channel, league, type = "text", msg = "No games for (league) today" } = obj;
+
+  const timeZone = Temporal.TimeZone.from('America/Sao_Paulo'); // Using Brazil time zone
   const todayDate = Temporal.Now.plainDateISO(timeZone).toString(); // Date for Brazil time zone
 
   try {
@@ -43,25 +42,29 @@ export async function GET(request) {
         const request = await fetch(vlrQuery(query), { method: "GET" });
         const response = await request.json();
         const segments = response.data.segments.map((segment) => {
+
           if (segment.match_event == validLeagues[league] || segment.tournament_name == validLeagues[league]) {
             if (!segment.unix_timestamp) {
-              segment.unix_timestamp = parseTimeCompleted(segment.time_completed);
-              segment.br_timestamp = segment.unix_timestamp;
+              // create unix_timestamp based on property "time_completed": "3h 41m ago"
+              segment.unix_timestamp = parseTimeCompleted(segment.time_completed); // 2024-12-12T19:59:48Z
             } else {
-              segment.unix_timestamp = `${segment.unix_timestamp.replace(" ", "T")}Z`;
-              segment.br_timestamp = segment.unix_timestamp;
+              // Format unix_timestamp to "2024-12-12T19:59:48Z"
+              segment.unix_timestamp = `${segment.unix_timestamp.replace(" ", "T")}Z`; // 2024-12-12T19:59:48Z
             }
 
-            const gameDateTime = timeZone.getPlainDateTimeFor(segment.unix_timestamp).toString();
-            const gameDate = timeZone.getPlainDateTimeFor(segment.unix_timestamp).toString().split('T')[0];
-            const gameTime = Temporal.PlainTime.from(gameDateTime).hour;
-            const gameMinute = Temporal.PlainTime.from(gameDateTime).minute;
+            const brGameDateTime = timeZone.getPlainDateTimeFor(segment.unix_timestamp).toString();
+            const gameMinute = Temporal.PlainTime.from(brGameDateTime).minute;
+
+            // If minute is greater than 40, add 1 hour. That avoids games that start at 16:59h show as started at 16h
             if (gameMinute > 40) {
-              segment.time_brazil = gameTime + 1;
+              segment.br_hour = Temporal.PlainTime.from(brGameDateTime).hour + 1; // gameTime + 1;
             } else {
-              segment.time_brazil = gameTime;
+              segment.br_hour = Temporal.PlainTime.from(brGameDateTime).hour // gameTime;
             }
-            segment.date_brazil = gameDate;
+
+            // Create br_timestamp using unix_timestamp and converting to Brazilian time
+            segment.br_timestamp = timeZone.getPlainDateTimeFor(segment.unix_timestamp).toString();
+            segment.br_date = segment.br_timestamp.split('T')[0]; // gameDate;
             return segment;
           };
         })
@@ -73,9 +76,8 @@ export async function GET(request) {
     // Check today games
     const todayGames = results.flat().map((game) => {
       const gameDate = timeZone.getPlainDateTimeFor(game.unix_timestamp).toString().split('T')[0];
-
       if (gameDate == todayDate) {
-        return game
+        return game;
       }
     }).filter(Boolean);
 
@@ -107,7 +109,7 @@ export async function GET(request) {
     return sendResponse(todayGames.flat(), 200, type, channel, league, msg)
 
   } catch (error) {
-    console.log.log(error)
+    console.log(error);
     return NextResponse.json({ error: { message: "An error has occurred. Please try again later.: ", code: error.code } }, { status: 500 });
   }
 }
@@ -121,8 +123,8 @@ async function checkParams(league, channel) {
 async function sendResponse(response, status, type, channel, league, msg) {
   const matches = [];
 
-  // First, sort the array by `time_brazil` in ascending order
-  response.sort((a, b) => a.time_brazil - b.time_brazil);
+  // First, sort the array by `br_hour` in ascending order
+  response.sort((a, b) => a.br_hour - b.br_hour);
 
   if (type == "text" && response.length > 0) {
     response.forEach((match) => {
@@ -130,8 +132,9 @@ async function sendResponse(response, status, type, channel, league, msg) {
       const nameTeam2 = match.team2;
       const winsTeam1 = match.score1 || 0;
       const winsTeam2 = match.score2 || 0;
-      matches.push(`${match.time_brazil}h - ${nameTeam1} ${winsTeam1}x${winsTeam2} ${nameTeam2}`)
+      matches.push(`${match.br_hour}h - ${nameTeam1} ${winsTeam1}x${winsTeam2} ${nameTeam2}`)
     })
+
     // Format the response like below:
     /* Team1 0x2 Team2 // Team3 2x0 Team4 // Team5 1x1 Team6 */
     const finalResponse = matches.toString().replaceAll(',', ' // ');
@@ -139,15 +142,11 @@ async function sendResponse(response, status, type, channel, league, msg) {
     return new Response(finalResponse, { status: 200 });
 
   } else if (type == "text" && response.length == 0) {
-
     // Created a new custom message and send it when there are no games
     const message = msg.replaceAll(/\(league\)/g, validLeagues[league]);
     console.log(message);
     return new Response(message, { status: 200 });
-
   }
 
-  // The below doesn't work because response is an array, not an object
-  // response.message = matches.length > 0 ? matches.toString().replaceAll(',', ' // ') : "No games for today";
   return NextResponse.json(response, { status: 200 });
 }
