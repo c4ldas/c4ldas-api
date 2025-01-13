a31d05f90b920983be851d40f0829b1cee66954f
Rodrigo Caldas <c4ldas@gmail.com>
2025-01-13 15:57:13 +0000

    Removed unused code

--- app/components/Linkbox.jsx
+++ app/components/Linkbox.jsx
@@ -48,20 +48,3 @@ export default function Linkbox(props) {
     </div>
   );
 }
-
-
-/* 
-import Link from 'next/link';
-import React from 'react'
-
-export default function Linkbox(props) {
-  return (
-    <div className="link-box">
-      <Link href={props.link} className="link-content">
-        <h3>{props.title}</h3>
-        <p className="description">{props.description}<span className={props.spanClass}>{props.span}</span></p>
-      </Link>
-    </div>
-  )
-}
- */
