602caf0c3ab4bb05cd85bb698997d7b5e83e88b4
Rodrigo Caldas <c4ldas@gmail.com>
2025-01-15 03:31:21 +0000

    Changed description

--- app/api/twitch/commercial/route.js
+++ app/api/twitch/commercial/route.js
@@ -1,19 +1,29 @@
 /*
-The idea of this endpoint is to create an option to run commercial when the Lol game is not running.
+The idea of this endpoint is to create an option to run commercial when a League of Legends match is not running.
 This will be done by a widget on Streamelements that will check if the game is finished, and if so, run the commercial.
 The widget will check the game status using Riot API through the /api/lol/active-game endpoint.
 
 The active-game endpoint will return a JSON object with the following structure:
 
 {
-  "inGame": true,
-  "info": {
-    "player": "playerName",
-    "tag": "playerTag",
-    "gameId": 3040033177,
-    "matchId": "BR1_3040033177",
-    "gameStartTime": 1734385418545,
-    "gameLength": 874
+  "inGame": false,
+  "player": "Minerva",
+  "tag": "BR1",
+  "currentGame": {
+    "gameId": 0,
+    "replayId": null,
+    "gameStartTime": 0,
+    "championName": null
+  },
+  "previousGame": {
+    "gameId": 3053457257,
+    "replayId": "BR1_3053457257",
+    "gameStartTime": 1736906589405,
+    "gameLength": 1941349,
+    "gameDuration": "32m 21s",
+    "result": "lose",
+    "championName": "Zyra",
+    "kda": "7/8/18"
   }
 }
 
