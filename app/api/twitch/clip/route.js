d9d5a1f222b14ca8c8523c3b3957e8cb77502573
Rodrigo Caldas <c4ldas@gmail.com>
2025-03-30 13:32:05 +0100

    Rewrite functions order

--- app/api/twitch/clip/route.js
+++ app/api/twitch/clip/route.js
@@ -1,6 +1,7 @@
 // Description:
 // - This file contains the route for the twitch clip endpoint
 // - The route will create a clip for the user and return the clip URL
+// - In case the type is "full" or "overlay", it will return the download URL as well
 // - The functions for the database are in the lib/database.js file
 // - The user that creates the clip is in the environment variable CLIP_CHANNEL
 
@@ -8,13 +9,16 @@ import { twitchGetTokenDatabase, twitchSaveToDatabase } from "@/app/lib/database
 import { createClip, editClipData, getClipData, getClipDownloadURL, getUserData } from "@/app/lib/twitch";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 25; // Vercel config for max duration of the route
+
 const CLIP_TWITCH_CLIENT_ID = process.env.CLIP_TWITCH_CLIENT_ID;
 const CLIP_TWITCH_CLIENT_SECRET = process.env.CLIP_TWITCH_CLIENT_SECRET;
 const CLIP_CHANNEL = process.env.CLIP_CHANNEL;
 const CLIP_CODE = process.env.CLIP_CODE;
-const clipBaseURL = "https://clips.twitch.tv/";
+const CLIP_BASE_URL = "https://clips.twitch.tv/";
 
-export const maxDuration = 25; // Vercel config for max duration of the route
+// type "full" of "overlay" should have the download link
+const validTypes = ["text", "json", "full", "overlay"];
 
 export async function GET(request) {
   try {
@@ -24,57 +28,59 @@ export async function GET(request) {
     // Check if the user is missing from the request
     if (!channel) return NextResponse.json({ status: "failed", message: "Missing channel" }, { status: 200 });
 
+    // Check if the type is valid
+    if (!validTypes.includes(type)) return NextResponse.json({ status: "failed", message: `Invalid type. 'type' should be one of ${validTypes.join(", ")}` }, { status: 200 });
+
     // Check if the duration is valid
-    if (isNaN(duration) || duration < 5 || duration > 30) return NextResponse.json({ status: "failed", message: "duration should be a number between 5 and 30" }, { status: 200 });
+    if (isNaN(duration) || duration < 5 || duration > 30) return NextResponse.json({ status: "failed", message: `Invalid duration. 'duration' should be a number between 5 and 30` }, { status: 200 });
 
-    // Get the token from c4ldasbot user
-    const token = await twitchGetTokenDatabase(CLIP_CODE, CLIP_CHANNEL);
-
-    // Check if the token is valid
+    // Get the token from c4ldasbot user and check if it is valid
+    let token = await twitchGetTokenDatabase(CLIP_CODE, CLIP_CHANNEL);
     if (!token) return NextResponse.json({ status: "failed", error: "Code and channel do not match" }, { status: 200 });
 
-    // Get a new token to avoid token expiration
-    const newToken = await getNewToken(token.refresh_token);
-    const newAccessToken = newToken.access_token;
+    // Get user data
+    let userData = await getUserData(token.access_token, channel);
 
-    // Data to save to database
-    const data = {
-      id: token.id,
-      username: CLIP_CHANNEL,
-      access_token: newToken.access_token,
-      refresh_token: newToken.refresh_token,
-      code: CLIP_CODE
+    if (userData.status == "failed") {
+      console.log(`Token expired for ${CLIP_CHANNEL}, getting a new one...`);
+      token = await getNewToken(token.refresh_token);
+
+      // Data to save to database
+      const data = {
+        id: token.id,
+        username: CLIP_CHANNEL,
+        access_token: token.access_token,
+        refresh_token: token.refresh_token,
+        code: CLIP_CODE
+      }
+
+      // Save to database
+      const saveToDatabase = await twitchSaveToDatabase(data);
+      if (!saveToDatabase) return NextResponse.json({ status: "failed", error: "Error while saving to database" }, { status: 200 });
+      userData = await getUserData(token.access_token, channel);
     }
 
-    // Save to database
-    const saveToDatabase = await twitchSaveToDatabase(data);
-    if (!saveToDatabase) return NextResponse.json({ status: "failed", error: "Error while saving to database" }, { status: 200 });
-
-    // Get user data
-    const userData = await getUserData(newAccessToken, channel);
-
     // Create a clip
-    // console.log(`Creating clip for ${channel}...`);
-    const clipData = await createClip(userData.id, newToken.access_token);
+    const clipData = await createClip(userData.id, token.access_token);
+    console.log(`Creating clip for ${channel}...`);
 
-    if (duration == 30) {
-      if (type === "text") return new Response(clipBaseURL + clipData.id, { status: 200 });
-      return NextResponse.json({ status: "success", data: { id: clipData.id, url: clipBaseURL + clipData.id } }, { status: 200 });
+    if (type != "full" && type != "overlay") {
+      if (type === "text") return new Response(CLIP_BASE_URL + clipData.id, { status: 200 });
+      if (type == "json") return NextResponse.json({ status: "success", data: { id: clipData.id, url: CLIP_BASE_URL + clipData.id } }, { status: 200 });
     }
 
     // Create a sleep to wait for the clip to be created
-    console.log(`Waiting for clip to be created for ${channel}...`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 4000));
 
     // Check if the clip is created
-    let clipFailed = 0;
-    let getClip = await getClipData(clipData.id, newToken.access_token);
+    let getClipFailed = 0;
+    let getClip = await getClipData(clipData.id, token.access_token);
 
-    while (!getClip && clipFailed < 3) {
-      clipFailed++;
+    while (!getClip && getClipFailed < 5) {
       console.log(`Clip not yet created for ${channel}, retrying...`);
       await new Promise((resolve) => setTimeout(resolve, 500));
-      getClip = await getClipData(clipData.id, newToken.access_token);
+      getClip = await getClipData(clipData.id, token.access_token);
+      getClipFailed++;
     }
 
     if (!getClip) return NextResponse.json({ status: "failed", message: "Failed to create clip, try again later" }, { status: 200 });
@@ -87,15 +93,13 @@ export async function GET(request) {
 
     // Edit the clip
     const editClip = await editClipData({ slug, assetId, title, duration, originalTitle });
-    // console.log(editClip);
     if (editClip[0].errors) { console.log("Error editing clip", editClip[0].data.error); }
-
-    if (type === "text") return new Response(clipURL, { status: 200 });
 
     // Get the download URL
     console.log(`Generating download URL for clip ${clipData.id}...`);
     await new Promise((resolve) => setTimeout(resolve, 4000));
     const downloadURL = await getClipDownloadURL(clipData.id);
+
     return NextResponse.json({ status: "success", data: { id: clipData.id, url: clipURL, video_url: downloadURL, title: editClip[0].data.editClipMedia.clip.title } }, { status: 200 });
 
   } catch (error) {
@@ -107,7 +111,6 @@ export async function GET(request) {
 export async function POST(request) {
   return NextResponse.json({ status: "failed", message: "Method not allowed (yet)" });
 }
-
 
 
 // Get a new token
