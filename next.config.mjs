76f06127413ffd538978fa3226c9ccc7ba70a4be
Rodrigo Caldas <c4ldas@gmail.com>
2025-04-15 18:57:46 +0100

    Add Spotify url

--- next.config.mjs
+++ next.config.mjs
@@ -38,6 +38,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'yt3.ggpht.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co'
+      }
     ],
   },
 };
