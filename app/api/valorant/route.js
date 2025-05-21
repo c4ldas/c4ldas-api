88b50a26edeac96f01f0f6dc4ba8da2276a03ac4
Rodrigo Caldas <c4ldas@gmail.com>
2025-05-21 23:48:44 +0100

    Add `/schedule/vlr/` route

--- app/api/valorant/route.js
+++ app/api/valorant/route.js
@@ -88,7 +88,7 @@ export async function GET(request) {
     {
       endpoint: `/api/valorant/schedule`,
       description: "Display the games of the day based on the tournament searched.",
-      usage: `${origin}${pathname}?channel=$(channel)&league=LEAGUE_NAME&type=TYPE`,
+      usage: `${origin}${pathname}/schedule?channel=$(channel)&league=LEAGUE_NAME&type=TYPE`,
       params: {
         channel: {
           description: "Your channel name, no need to change",
@@ -118,43 +118,37 @@ export async function GET(request) {
           available_variables: ["json", "text"],
         }
       }
+    },
+    {
+      endpoint: `/api/valorant/schedule/vlr`,
+      description: "Display the games of the day from vlr.gg.",
+      usage: `${origin}${pathname}schedule/vlr?channel=$(channel)&league=LEAGUE_NAME&type=TYPE`,
+      params: {
+        channel: {
+          description: "Your channel name, no need to change",
+          required: true
+        },
+        league: {
+          description: "Name of the league",
+          required: true,
+          available_variables: ["tixinha_invitational", "vcb", "vct_americas", "vct_emea", "masters_bangkok", "masters_toronto", "valorant_champions_2025"],
+          additional_info: "More leagues can be added, just contact me requesting the name of the league and the link of the vlr.gg page."
+        },
+        type: {
+          description: "Format of the response",
+          required: false,
+          default: "text",
+          available_variables: ["json", "text"],
+        },
+        msg: {
+          description: "Message to be displayed as response when no game is found",
+          required: false,
+          default: "No games for (league) today",
+          available_variables: ["(league)"],
+        },
+      }
     }
   ]
   );
 };
 
-
-/* 
-{
-  endpoint: `/api/lol/rank`,
-  description: "Display your real-time rank and elo in League of Legends.",
-  usage: `${origin}${pathname}/rank?player=PLAYERNAME&tag=TAG&region=REGION&type=TYPE&msg=MSG`,
-  params: {
-    player: {
-      description: "Name of the player",
-      required: true,
-    },
-    tag: {
-      description: "Tag of the player, same as #",
-      required: true,
-    },
-    region: {
-      description: "Region of the player",
-      required: true,
-      available_variables: ["eun1", "euw1", "ru1", "tr1", "br1", "la1", "la2", "na1", "jp1", "kr", "oc1"]
-    },
-    type: {
-      description: "Format of the response",
-      required: false,
-      default: "text",
-      available_variables: ["json", "text"],
-    },
-    msg: {
-      description: "Message to be displayed as response",
-      required: false,
-      default: "(player): (rank) - (points) points",
-      available_variables: ["(player)", "(tag)", "(rank)", "(points)", "(wins)", "(losses)"],
-    },
-  }
-}
-*/
