a1216aa456ab1c98ea933599a9ac424451734976
Rodrigo Caldas <c4ldas@gmail.com>
2025-07-02 02:51:13 +0100

    Add custom auth response

--- app/api/twitch/callback/route.js
+++ app/api/twitch/callback/route.js
@@ -1,11 +1,12 @@
 import { twitchSaveToDatabase, twitchCheckUser } from "@/app/lib/database";
 import { getTokenCode, getUserData } from "@/app/lib/twitch";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
   const obj = Object.fromEntries(request.nextUrl.searchParams);
-  const { code } = obj;
+  const { code, state } = obj;
   const origin = request.nextUrl.origin;
 
   if (!code) return Response.redirect(`${origin}/twitch?error=Code not found`);
@@ -25,6 +26,10 @@ export async function GET(request) {
 
   const saved = await twitchSaveToDatabase(data);
   if (!saved) return Response.redirect(`${origin}/twitch?error=Error while saving to database`);
+
+  if (state === 'custom_auth_twitch') {
+    return NextResponse.json({ status: "success", data: data }, { status: 200 });
+  }
 
   cookies().set('twitch_id', data.id);
   cookies().set('twitch_username', data.username);
