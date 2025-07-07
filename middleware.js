54aacc4f0b5562f5c960c8db265344b05402da89
Rodrigo Caldas <c4ldas@gmail.com>
2025-07-07 22:44:38 +0100

    Add middleware.js

---
+++ middleware.js
@@ -0,0 +1,18 @@
+// middleware.js
+import { NextResponse } from 'next/server';
+import { track } from '@vercel/analytics/server';
+
+// Get the channel name from the query string for each API request
+export async function middleware(request) {
+  const { pathname } = request.nextUrl;
+  const obj = Object.fromEntries(request.nextUrl.searchParams);
+
+  track('channelName', {
+    channel: obj.channel ?? "none",
+    path: pathname
+  });
+
+  return NextResponse.next();
+}
+
+export const config = { matcher: '/api/:path*' }
