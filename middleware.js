594c87c3f2436b1979d6f5dd745b126337d6fa50
Rodrigo Caldas <c4ldas@gmail.com>
2025-07-07 23:47:23 +0100

    Remove Analytics

--- middleware.js
+++ middleware.js
@@ -1,17 +1,17 @@
 // middleware.js
 import { NextResponse } from 'next/server';
-import { track } from '@vercel/analytics/server';
+// import { track } from '@vercel/analytics/server';
 
 // Get the channel name from the query string for each API request
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
   const obj = Object.fromEntries(request.nextUrl.searchParams);
-
+  /* 
   track('channelName', {
     channel: obj.channel ?? "none",
     path: pathname
   });
-
+ */
   return NextResponse.next();
 }
 
