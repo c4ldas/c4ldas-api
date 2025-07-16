e26332203cf1667680fdca69390209dbf8a66dd4
Rodrigo Caldas <c4ldas@gmail.com>
2025-07-16 08:03:03 +0100

    Create EWC schedule endpoint

---
+++ app/api/lol/schedule/ewc/route.js
@@ -0,0 +1,87 @@
+import { NextResponse } from "next/server";
+
+const baseURL = "https://dzuvpjnlus2rx.cloudfront.net/2025-03-11/graphql";
+const operationName = "MatchSeries";
+const variables = `{ "tournamentIds": ["66936630-ec85-4f1b-9e67-325d0389eb87", "13bd2b48-922d-4800-8df9-1e9f646221be"] }`;
+const extensions = `{ "persistedQuery": { "version": 1, "sha256Hash": "48a028dd531ba27242b908d22c175384e78014e53615d6c933200b43a1fc29c2" } }`;
+let leagueName = "League of Legends EWC";
+
+export async function GET(request) {
+  const obj = Object.fromEntries(request.nextUrl.searchParams);
+  const { channel, type = "text", msg = "No games for (league) today" } = obj;
+
+  try {
+    const gameRequest = await fetch(`${baseURL}?` +
+      new URLSearchParams({
+        "operationName": operationName,
+        "variables": variables,
+        "extensions": extensions
+      }),
+      { method: "GET" }
+    );
+
+    if (!gameRequest.ok) {
+      return sendResponse(gameRequest, error = true);
+    }
+
+    const response = await gameRequest.json();
+    const matches = listTodayMatches(response);
+
+    return sendResponse({ matches, type, channel, msg });
+
+  } catch (error) {
+    console.log("Error!", error.message);
+    return NextResponse.json({ status: "failed", message: "Failed to list League of Legends EWC matches. Please try again." }, { status: 200 });
+  }
+}
+
+
+function listTodayMatches(data) {
+  leagueName = data.data.matchSeries.items[0].tournament.name;
+  const matchList = data.data.matchSeries.items;
+
+  const matches = [];
+
+  for (let i = 0; i < matchList.length; i++) {
+    const match = matchList[i];
+    const matchDate = new Date(match.startTime);
+    const today = new Date();
+    if (matchDate.getDate() == today.getDate()) {
+      const game = { startTime: match.startTime, team1Name: match.contestants[0]?.team.name, team2Name: match.contestants[1]?.team.name, team1Score: match.contestants[0]?.score, team2Score: match.contestants[1]?.score }
+      if (!game.team1Name || !game.team2Name) continue;
+      matches.push(game);
+    }
+  }
+  return matches;
+}
+
+
+// Placeholder function, need to be implemented
+async function sendResponse(data) {
+  const { matches, type, channel, msg } = data;
+
+  console.log(matches);
+
+  const games = matches.map((match) => {
+    // Get hours and append an 'h' if needed
+    const hour = match.startTime.split("T")[1].split(":")[0] - 3;
+    return `${hour}h - ${match.team1Name} ${match.team1Score}x${match.team2Score} ${match.team2Name}`
+  });
+
+  const message = msg.replaceAll(/\(league\)/g, leagueName);
+
+  if (type != "text") {
+    console.log(games.join(" // "));
+    console.log(leagueName);
+    return NextResponse.json({ matches, title: leagueName }, { status: 200 });
+  }
+
+  if (type == "text" && matches.length == 0) {
+    console.log(message);
+    return new Response(message, { status: 200 });
+  }
+
+  console.log(leagueName);
+  console.log(games.join(" // "));
+  return new Response(games.join(" // "), { status: 200 });
+}
