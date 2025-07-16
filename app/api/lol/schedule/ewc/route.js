be142711e2b66c757436d1720d7a575ab5d254bf
Rodrigo Caldas <c4ldas@gmail.com>
2025-07-16 08:30:51 +0100

    Refactor sendResponse

--- app/api/lol/schedule/ewc/route.js
+++ app/api/lol/schedule/ewc/route.js
@@ -4,7 +4,7 @@ const baseURL = "https://dzuvpjnlus2rx.cloudfront.net/2025-03-11/graphql";
 const operationName = "MatchSeries";
 const variables = `{ "tournamentIds": ["66936630-ec85-4f1b-9e67-325d0389eb87", "13bd2b48-922d-4800-8df9-1e9f646221be"] }`;
 const extensions = `{ "persistedQuery": { "version": 1, "sha256Hash": "48a028dd531ba27242b908d22c175384e78014e53615d6c933200b43a1fc29c2" } }`;
-let leagueName = "League of Legends EWC";
+let title = "League of Legends EWC";
 
 export async function GET(request) {
   try {
@@ -40,7 +40,7 @@ export async function GET(request) {
 
 
 function listTodayMatches(data) {
-  leagueName = data.data.matchSeries.items[0].tournament.name;
+  title = data.data.matchSeries.items[0].tournament.name;
   const matchList = data.data.matchSeries.items;
 
   const matches = [];
@@ -50,7 +50,15 @@ function listTodayMatches(data) {
     const matchDate = new Date(match.startTime);
     const today = new Date();
     if (matchDate.getDate() == today.getDate()) {
-      const game = { startTime: match.startTime, team1Name: match.contestants[0]?.team.name, team2Name: match.contestants[1]?.team.name, team1Score: match.contestants[0]?.score, team2Score: match.contestants[1]?.score }
+      const hour = match.startTime.split("T")[1].split(":")[0] - 3;
+      const game = {
+        startTime: match.startTime,
+        team1Name: match.contestants[0]?.team.name,
+        team2Name: match.contestants[1]?.team.name,
+        team1Score: match.contestants[0]?.score,
+        team2Score: match.contestants[1]?.score,
+        message: `${hour}h - ${match.contestants[0]?.team.name} ${match.contestants[0]?.score}x${match.contestants[1]?.score} ${match.contestants[1]?.team.name}`
+      }
       if (!game.team1Name || !game.team2Name) continue;
       matches.push(game);
     }
@@ -58,25 +66,16 @@ function listTodayMatches(data) {
   return matches;
 }
 
-
-// Placeholder function, need to be implemented
 async function sendResponse(data) {
   const { matches, type, channel, msg } = data;
 
-  console.log(matches);
-
-  const games = matches.map((match) => {
-    // Get hours and append an 'h' if needed
-    const hour = match.startTime.split("T")[1].split(":")[0] - 3;
-    return `${hour}h - ${match.team1Name} ${match.team1Score}x${match.team2Score} ${match.team2Name}`
-  });
-
-  const message = msg.replaceAll(/\(league\)/g, leagueName);
+  const games = matches.map((match) => match.message);
+  const message = msg.replaceAll(/\(league\)/g, title);
 
   if (type != "text") {
     console.log(games.join(" // "));
-    console.log(leagueName);
-    return NextResponse.json({ matches, title: leagueName }, { status: 200 });
+    console.log(title);
+    return NextResponse.json({ matches, title }, { status: 200 });
   }
 
   if (type == "text" && matches.length == 0) {
@@ -84,7 +83,7 @@ async function sendResponse(data) {
     return new Response(message, { status: 200 });
   }
 
-  console.log(leagueName);
+  console.log(title);
   console.log(games.join(" // "));
   return new Response(games.join(" // "), { status: 200 });
 }
