171ea1e4e936188774f9860da3fcb10719d671e9
Rodrigo Caldas <c4ldas@gmail.com>
2025-07-25 23:48:07 +0100

    Add items to header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,6 +20,15 @@ export default function Header() {
           placeholder="empty"
         />
       </Link>
+      <div className="header-links">
+        <Link id="home" href="/" className="home"><div style={{ color: "var(--color)" }}>Home</div></Link>
+        <Link id="twitch" href="/twitch" className="twitch"><div style={{ color: "var(--color)" }}>Twitch</div></Link>
+        <Link id="tft" href="/tft" className="tft"><div style={{ color: "var(--color)" }}>TFT</div></Link>
+        <Link id="lol" href="/lol" className="lol"><div style={{ color: "var(--color)" }}>League of Legends</div></Link>
+        <Link id="valorant" href="/valorant" className="valorant"><div style={{ color: "var(--color)" }}>Valorant</div></Link>
+        <Link id="spotify" href="/spotify" className="spotify"><div style={{ color: "var(--color)" }}>Spotify</div></Link>
+        <Link id="youtube" href="/youtube" className="youtube"><div style={{ color: "var(--color)" }}>Youtube</div></Link>
+      </div>
       <DarkMode />
     </header>
   )
