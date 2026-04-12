325dff8a737af0ef9cec2e6ccc52b0a00a7d1680
Rodrigo Caldas <c4ldas@gmail.com>
2026-04-12 06:14:49 +0100

    Add custom msg query parameter

--- app/api/valorant/puuid/route.js
+++ app/api/valorant/puuid/route.js
@@ -8,17 +8,20 @@ const apiToken = env == "dev" ?
   process.env.VALORANT_TOKEN;
 
 const urlByPlayer = (player, tag) => `https://api.henrikdev.xyz/valorant/v1/account/${player}/${tag}?force=true`;
+const urlByPuuid = (puuid) => `https://api.henrikdev.xyz/valorant/v1/by-puuid/account/${puuid}?force=true`;
 
 export async function GET(request) {
   // Convert query strings (map format) to object format - Only works for this specific case!
   const obj = Object.fromEntries(request.nextUrl.searchParams);
   try {
     // Get the parameters from URL
-    const { player = "", tag = "", type = "json" } = obj;
+    const { player = "", tag = "", type = "text", puuid = "", msg = "(puuid)" } = obj;
 
-    const validParams = checkParams(player, tag);
+    const validParams = checkParams(player, tag, puuid);
 
-    const getData = await fetch(urlByPlayer(player, tag), {
+    const url = puuid ? urlByPuuid(puuid) : urlByPlayer(player, tag);
+
+    const getData = await fetch(url, {
       headers: {
         "Authorization": apiToken
       }
@@ -27,9 +30,7 @@ export async function GET(request) {
 
     const response = await getData.json();
 
-    if (type === "json") return NextResponse.json(response, { status: 200 });
-
-    return new Response((response.data.puuid), { status: 200 });
+    return sendResponse(response, type, msg);
 
   } catch (error) {
     console.log(error);
@@ -37,8 +38,26 @@ export async function GET(request) {
   }
 }
 
-function checkParams(player, tag) {
-  if (!player || !tag) throw ({ error: { message: "Missing player / tag", player: player, tag: tag, status: 200 } });
+function checkParams(player, tag, puuid) {
+  if ((!player || !tag) && (!puuid)) throw ({ error: { message: "Missing player / tag or puuid", player: player, tag: tag, puuid: puuid, status: 200 } });
   return { status: true, error: null };
 }
 
+
+async function sendResponse(data, type, msg) {
+
+  const { name, tag, puuid, account_level } = data.data;
+  const formattedMessage = msg
+    .replace(/\(player\)/g, name)
+    .replace(/\(tag\)/g, tag)
+    .replace(/\(puuid\)/g, puuid)
+    .replace(/\(level\)/g, account_level);
+
+  if (type != "text") {
+    data.message = formattedMessage;
+    console.log(formattedMessage);
+    return NextResponse.json(data, { status: 200 });
+  }
+  console.log(formattedMessage);
+  return new Response(formattedMessage, { status: 200 });
+}
